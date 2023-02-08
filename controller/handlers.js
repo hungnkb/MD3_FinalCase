defd685d3b1bdf37cfa15725530cfc9c9e8edc8d
unknown <hungnkb>
2023-02-08 16:54:29 +0700

    login/out

--- controller/handlers.js
+++ controller/handlers.js
@@ -3,6 +3,8 @@ const getTemplate = require('../model/getTemplate');
 const fs = require('fs');
 const qs = require('qs');
 const formidable = require('formidable');
+const query = require('../model/query');
+var cookie = require('cookie');
 
 let handlers = {};
 
@@ -27,11 +29,53 @@ handlers.login = async (req, res) => {
     });
     req.on('end', async () => {
         data = qs.parse(data);
-        console.log(data);
 
-        
+        let sqlCheckLogin = `select username, role from account where username = '${data.username}' and password = '${data.password}'`;
+        let resultCheckLogin = await query.selectProduct(sqlCheckLogin);
+        if (resultCheckLogin.length == 0) {
+            res.writeHead(301, { 'Location': '/login' });
+            res.end();
+        } else {
+            let fileSession = resultCheckLogin[0].username + '.txt';
+            let dataSession = JSON.stringify(resultCheckLogin[0]);
+
+            fs.writeFile('./sessions/' + fileSession, dataSession, err => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.setHeader('Set-Cookie', [
+                        cookie.serialize("u_user", resultCheckLogin[0].username),
+                        cookie.serialize("u_role", resultCheckLogin[0].role)
+                    ])
+
+                    //role == 0 => admin;
+                    // role == 1 => user; 
+                    if (resultCheckLogin[0].role == 0) {
+                        res.writeHead(301, { 'Location': '/product' });
+                        res.end();
+                    } else {
+                        res.writeHead(301, { 'Location': '/home' });
+                        res.end();
+                    }
+
+                }
+            })
+        }
+    });
+
+}
+
+handlers.logout = (req, res) => {
+    let cookies = req.headers.cookie;
+    let dataCookie = cookie.parse(cookies);
+    let nameSession = dataCookie.u_user;
+    fs.unlink('./sessions/' + nameSession + '.txt', () => {
+        res.writeHead(301, { 'Location': '/login' });
+        res.end();
     })
 }
+
+
 
 
 module.exports = { handlers };
