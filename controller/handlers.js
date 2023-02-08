4b1d4f2fe7f0717a82600992f6e084a231536087
unknown <hungnkb>
2023-02-08 15:39:07 +0700

    2nd

--- controller/handlers.js
+++ controller/handlers.js
@@ -6,8 +6,32 @@ const formidable = require('formidable');
 
 let handlers = {};
 
+handlers.showHomePage = async (req, res) => {
+    let html = await getTemplate.readHtml('./view/home.html')
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(html);
+    res.end();
+}
 
+handlers.showLogin = async (req, res) => {
+    let html = await getTemplate.readHtml('./view/user/login.html')
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(html);
+    res.end();
+}
 
+handlers.login = async (req, res) => {
+    let data = '';
+    req.on('data', chunk => {
+        data += chunk;
+    });
+    req.on('end', async () => {
+        data = qs.parse(data);
+        console.log(data);
+
+        
+    })
+}
 
 
 module.exports = { handlers };
